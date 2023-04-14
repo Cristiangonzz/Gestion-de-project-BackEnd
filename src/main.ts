02022d8a23835bf52922291f2b43603f6fe349e9
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-04-14 04:25:14 -0300

    Se coloco la ruta de fireBase en el backend para el Cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://project-management-2a01a.web.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   });
